c6fa70f413b996ef5e04cb189cb0ebdaa4bcf50d
voduchuy2001 <voduchuy2001@gmail.com>
2023-04-04 15:39:52 +0700

    Realtime chat implementation

--- app/src/main/java/vdhuy/myapp/models/ChatMessage.java
+++ app/src/main/java/vdhuy/myapp/models/ChatMessage.java
@@ -1,5 +1,8 @@
 package vdhuy.myapp.models;
 
+import java.util.Date;
+
 public class ChatMessage {
     public String senderId, receiverId, message, dateTime;
+    public Date dateObject;
 }
--- app/src/main/java/vdhuy/myapp/models/User.java
+++ app/src/main/java/vdhuy/myapp/models/User.java
@@ -3,5 +3,5 @@ package vdhuy.myapp.models;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    public String name, image, email, token;
+    public String name, image, email, token, id;
 }
